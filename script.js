0cfddb740db88720ebc6bdee6dbffebaa03374c3
JoaldoSanth <joaldosant3@gmail.com>
2024-04-30 01:38:56 -0300

    projeto de HTML e CSS com JavaScript

---
+++ script.js
@@ -0,0 +1,29 @@
+document
+  .getElementsByClassName("fa fa-chrome")[0]
+  .addEventListener("click", () => {
+    window.open("https://www.hltv.org/news/38877/fallen-with-more-autonomy-and-responsibility-i-really-want-to-leave-my-mark-here-during-the-next-year");
+  });
+
+document
+  .getElementsByClassName("fab fa-youtube")[1]
+  .addEventListener("click", () => {
+    window.open("https://www.hltv.org/news/38875/nicoodoz-im-not-satisfied-or-happy-at-all-with-my-performance-so-far");
+  });
+
+document
+  .getElementsByClassName("fab fa-youtube")[2]
+  .addEventListener("click", () => {
+    window.open("https://www.hltv.org/news/38874/dycha-initiates-role-shift-its-disgusting-when-we-lose-matches-because-of-me");
+  });
+
+document.getElementsByClassName("article")[0].addEventListener("click", () => {
+  window.open("https://www.hltv.org/news/38878/sl3nd-completes-move-to-gamerlegion");
+});
+
+document.getElementsByClassName("article")[1].addEventListener("click", () => {
+  window.open("https://www.hltv.org/news/38869/g2-make-epl-playoffs-after-thriller-revenge-series-against-m80");
+});
+
+document.getElementsByClassName("article")[2].addEventListener("click", () => {
+  window.open("https://www.hltv.org/news/38866/mibr-win-esl-challenger-melbourne-qualify-for-esl-pro-league-season-20");
+});
